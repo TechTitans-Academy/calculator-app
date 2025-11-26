3b1f2d9f502985e8b99bb665413a6236af9ab21c
Tech Titans! 🏆 <learnwithtechtitans@gmail.com>
2025-11-26 19:06:38 +0530

    Add simple program to add two numbers in Python

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Simple Python program to add two numbers
+
+# taking inputs
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# addition
+result = num1 + num2
+
+# output
+print("The sum of the number is:", result)
