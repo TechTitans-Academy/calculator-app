2d64a86c1adca2e191beb38800501df32fd682df
Tech Titans! 🏆 <learnwithtechtitans@gmail.com>
2025-11-26 19:58:01 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,9 +1,4 @@
-# taking inputs
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-
-# addition
 result = num1 + num2
-
-# output
 print("The Addition of the number is:", result)
